0cf5df89095de4292bcb04a9460eeb424998e038
David Enamorado <david8enaceituno@gmail.com>
2022-01-30 22:55:35 -0600

    commit initial

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:app_components/screens/listview1_screeen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App components',
+        home: Listview1Screen());
+  }
+}
