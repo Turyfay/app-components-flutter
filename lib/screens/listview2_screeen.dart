aae01406a86bd5d8ea1d6b7de3e4d93460e73588
David Enamorado <david8enaceituno@gmail.com>
2022-01-30 23:34:35 -0600

    Tips ListTitle y appBar

--- lib/screens/listview2_screeen.dart
+++ lib/screens/listview2_screeen.dart
@@ -8,12 +8,18 @@ class Listview2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Lista de componetes')),
+        appBar: AppBar(
+          title: const Text('Lista de componetes'),
+          elevation: 0,
+          backgroundColor: Colors.deepPurple,
+        ),
         body: ListView.separated(
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text(options[index]),
-                trailing: const Icon(Icons.arrow_forward_ios),
+                trailing:
+                    const Icon(Icons.arrow_forward_ios, color: Colors.indigo),
+                onTap: () {},
               );
             },
             separatorBuilder: (_, __) => const Divider(),
