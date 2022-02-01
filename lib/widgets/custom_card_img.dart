f045613300333f646c49fa07abafe7f25b0f0c4d
David Enamorado <david8enaceituno@gmail.com>
2022-02-01 00:27:45 -0600

    Se creo el widget de card img

--- lib/widgets/custom_card_img.dart
+++ lib/widgets/custom_card_img.dart
@@ -6,12 +6,25 @@ class CustomCardImgScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage(
                 'https://www.online-image-editor.com/styles/2019/images/power_girl.png'),
             placeholder: AssetImage('assets/loading/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 100),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('Super Girl'),
           )
         ],
       ),
