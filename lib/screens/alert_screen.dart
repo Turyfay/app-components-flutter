6dfa830d10adaf3b2f1af62743e29ef810b2025f
David Enamorado <david8enaceituno@gmail.com>
2022-02-01 01:41:52 -0600

    Mostrando dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,21 +3,47 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(context) {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            elevation: 5,
+            title: const Text('Alert Dialog'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('This is an alert dialog'),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('OK')),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Alert')),
+      //appBar: AppBar(title: const Text('Alert')),
       body: Center(
         child: ElevatedButton(
             /* style: ElevatedButton.styleFrom(
               primary: Colors.indigo,
             ), */
-            onPressed: () => null,
+            onPressed: () => displayDialog(context),
             child: const Text('Show Alert')),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () => null,
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
