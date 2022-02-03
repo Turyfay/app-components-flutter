74b0843d5684112e85057c51fb0a50ecc7cbd7fd
David Enamorado <david8enaceituno@gmail.com>
2022-02-02 23:34:18 -0600

    Checkbox y Swich widgets

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -8,7 +8,8 @@ class SliderScreen extends StatefulWidget {
 }
 
 class _SliderScreenState extends State<SliderScreen> {
-  double _value = 50.0;
+  double _value = 100.0;
+  bool _showValue = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,29 +17,63 @@ class _SliderScreenState extends State<SliderScreen> {
         appBar: AppBar(
           title: const Text('Slider'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Slider.adaptive(
-                  min: 0.0,
-                  max: 500.0,
-                  divisions: 10,
-                  value: _value,
-                  onChanged: (double value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  }),
-              Image(
-                image: const NetworkImage('https://picsum.photos/250?image=20'),
-                fit: BoxFit.contain,
-                width: _value,
+        body: Column(
+          children: [
+            Slider.adaptive(
+                min: 0.0,
+                max: 700.0,
+                divisions: 10,
+                value: _value,
+                onChanged: _showValue
+                    ? (value) {
+                        _value = value;
+                        setState(() {});
+                      }
+                    : null),
+            Checkbox(
+                value: _showValue,
+                onChanged: (value) {
+                  setState(() {
+                    _showValue = value ?? true;
+                  });
+                }),
+            CheckboxListTile(
+                activeColor: Colors.red,
+                title: const Text('Habilitar'),
+                value: _showValue,
+                onChanged: (value) {
+                  setState(() {
+                    _showValue = value ?? true;
+                  });
+                }),
+            Switch(
+                value: _showValue,
+                onChanged: (value) {
+                  setState(() {
+                    _showValue = value;
+                  });
+                }),
+            SwitchListTile.adaptive(
+                title: const Text('Habilitar'),
+                activeColor: Colors.red,
+                value: _showValue,
+                onChanged: (value) {
+                  setState(() {
+                    _showValue = value;
+                  });
+                }),
+            const AboutListTile(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage(
+                      'https://c.wallhere.com/photos/23/52/Fate_Series_FGO_Fate_Stay_Night_anime_girls_long_hair_small_boobs_blond_hair_green_eyes-1739895.jpg!d'),
+                  fit: BoxFit.contain,
+                  width: _value,
+                ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }
