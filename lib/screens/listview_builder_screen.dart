4f97f12c19d04eabf09e2fba63f6b14a701d0a63
David Enamorado <david8enaceituno@gmail.com>
2022-02-03 00:09:02 -0600

    Infiniti Scroll

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ListviewBuilderScreen extends StatelessWidget {
+class ListviewBuilderScreen extends StatefulWidget {
   const ListviewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListviewBuilderScreen> createState() => _ListviewBuilderScreenState();
+}
+
+class _ListviewBuilderScreenState extends State<ListviewBuilderScreen> {
+  final List<int> imageIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels + 200 >=
+          _scrollController.position.maxScrollExtent) {
+        _loadMore();
+      }
+    });
+  }
+
+  void _loadMore() {
+    setState(() {
+      final int index = imageIds.length;
+      imageIds.addAll(
+          [11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((e) => index + e));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        controller: _scrollController,
+        itemCount: imageIds.length,
         itemBuilder: (BuildContext context, int index) {
           return FadeInImage(
               width: double.infinity,
               height: 300,
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/loading/jar-loading.gif'),
-              image:
-                  NetworkImage('https://picsum.photos/500/300?image=$index'));
+              image: NetworkImage(
+                  'https://picsum.photos/500/300?image=${imageIds[index]}'));
         },
       ),
     );
